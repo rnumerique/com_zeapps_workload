6bbb9f3c19baf42ef5e8517ef043cea610f510ab
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-02-17 13:20:47 +0100

    Bugfix

--- models/Zeapps_workload_workloads.php
+++ models/Zeapps_workload_workloads.php
@@ -3,10 +3,10 @@
 class Zeapps_workload_workloads extends ZeModel
 {
     public function updateOldTable($status, $position) {
-        $this->db->query('UPDATE zeapps_workload_workloads SET position = (position-1) WHERE status = ' . $status . ' AND position > ' . $position);
+        $this->database()->query('UPDATE zeapps_workload_workloads SET position = (position-1) WHERE status = ' . $status . ' AND position > ' . $position);
     }
 
     public function updateNewTable($status, $position) {
-        $this->db->query('UPDATE zeapps_workload_workloads SET position = (position+1) WHERE status = ' . $status . ' AND position >= ' . $position);
+        $this->database()->query('UPDATE zeapps_workload_workloads SET position = (position+1) WHERE status = ' . $status . ' AND position >= ' . $position);
     }
 }
